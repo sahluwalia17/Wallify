d4921db71af7f88bcbb25edee2b55bd12cbdb306
sahluwalia17 <sahilahluwalia3@gmail.com>
2019-04-17 00:39:11 -0400

    downloading stock image to directory and reading it from the html file

--- request.py
+++ request.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    url = 'https://www.purdue.edu/purdue/images/audience/about-banner.jpg'
+    urllib.request.urlretrieve(url, "./static/Purdue.jpg")
+
     return render_template("index.html")
 
 if __name__ == "__main__":
