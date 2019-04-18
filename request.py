f0ce99a9e3328fa86f46441b42546b025158579e
sahluwalia17 <sahilahluwalia3@gmail.com>
2019-04-17 22:34:33 -0400

    auth token received

--- request.py
+++ request.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template
-import urllib.request
+from flask import Flask, render_template, redirect
+from urllib.parse import quote
+import webbrowser
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    url = 'https://www.purdue.edu/purdue/images/audience/about-banner.jpg'
-    urllib.request.urlretrieve(url, "./static/Purdue.jpg")
+def index():
+    clientId = "45ba6741126e4af1b9c7fef7f6bd7568"
+    baseURL = "https://accounts.spotify.com/authorize"
+    redirectURL = "http://127.0.0.1:5000/callback/q"
+    scope = "user-top-read"
 
+    auth_query_parameters = {
+        "response_type": "code",
+        "redirect_uri": redirectURL,
+        "scope": scope,
+        "client_id": clientId
+    }
+
+    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
+    auth_url = "{}/?{}".format(baseURL, url_args)
+
+    return redirect(auth_url)
+
+@app.route("/")
+def callback():
     return render_template("index.html")
 
 if __name__ == "__main__":
