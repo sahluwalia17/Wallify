f5f50ca5ed95fb65a4f88d635ee675818ff98756
sahluwalia17 <sahilahluwalia3@gmail.com>
2019-05-13 16:04:18 -0400

    unique token generated

--- static/function.js
+++ static/function.js
@@ -1,9 +1,9 @@
 var ints = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
 var dragId;
 
-function init(vars)
+function init()
 {
-  console.log(vars)
+  console.log(Math.floor((Math.random() * 100) + 1))
 }
 
 function allowDrop(ev) {
