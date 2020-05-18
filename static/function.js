44cb6875c65232d60c00276d27af1a4a9472576e
Puja Maheshwari <pujam1231@gmail.com>
2020-05-18 14:20:02 -0700

    fixing download array

--- static/function.js
+++ static/function.js
@@ -2,26 +2,14 @@ var ints = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
 var tokenARR = [];
 var dragId;
 
-function init()
-{
-  token = Math.floor((Math.random() * 1000) + 1);
-  tokenARR.push(token);
-  $.ajax({
-    type: "POST",
-    contentType: "application/json;charset=utf-8",
-    url: "/receive_token",
-    traditional: "true",
-    data: JSON.stringify({tokenARR}),
-    dataType: "json"
-    });
-}
-
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function drag(dragEvent) {
-  dragEvent.dataTransfer.setData("Id",    dragEvent.target.id+"|"+dragEvent.target.parentNode.id);
+  console.log("drag: ");
+  console.log(dragEvent);
+  dragEvent.dataTransfer.setData("Id", dragEvent.target.id+"|"+dragEvent.target.parentNode.id);
   dragId = dragEvent.target.id;
 }
 
@@ -60,8 +48,12 @@ function drop(dropEvent) {
   var swap1 = parseInt(dropEvent.target.id.substring(3));
   var swap2 = parseInt(dragId.substring(3));
   var temp = swap1;
-  ints[ints.indexOf(swap1)] = swap2;
-  ints[ints.indexOf(swap2)] = temp;
+  inswap1 = ints.indexOf(swap1);
+  inswap2 = ints.indexOf(swap2);
+  // ints[ints.indexOf(swap1)] = swap2;
+  ints[inswap1] = swap2;
+  //ints[ints.indexOf(swap2)] = temp;
+  ints[inswap2] = temp;
   prevElem.getElementsByTagName("div")[0].id = dropEvent.target.id;
   dropEvent.target.id = dropItems[0];
   dropEvent.preventDefault();
