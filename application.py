5dfa57926c0a3ab01f08593a0ec774e13daf8426
Puja Maheshwari <pujam1231@gmail.com>
2020-06-17 15:35:21 -0700

    changes for intermediate in

--- application.py
+++ application.py
@@ -193,10 +193,13 @@ def spotify(spotifyAPI):
 def intermediate():
     #perform redirects based on the option that the user picks
     if request.method == "POST":
+        print(request.form.getlist('option'))
         if request.form["option"] == "Recent Bops":
             trackinfo = spotify("https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50")
             return redirect(url_for('short',data=trackinfo))
         elif request.form["option"] == "Semester Jams":
+            print("HEREEE")
+
             trackinfo = spotify("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50")
             return redirect(url_for('medium',data=trackinfo))
         elif request.form["option"] == "Run It Back Turbo":
